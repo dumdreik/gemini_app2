4e884aac72cd4d1cd758e6e09af538ab9c2c8619
e.semichastnov <e.semichastnov@web-hands.ru>
2024-06-27 17:52:03 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
 
-function App(props) {
+function App() {
 	return (
 		<>
 			<Sidebar/>
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let elRoot = document.getElementById('root');
+let root = ReactDOM.createRoot(elRoot);
 root.render(
   <React.StrictMode>
     <App />
